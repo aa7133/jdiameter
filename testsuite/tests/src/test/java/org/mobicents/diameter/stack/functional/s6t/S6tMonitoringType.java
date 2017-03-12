5b56e1593143ce4b024d907a4b8c930111f7be39
adienzel <adienzel@gmail.com>
2017-03-12 12:15:16 +0000

    Add enumurator for monitoring type

---
+++ testsuite/tests/src/test/java/org/mobicents/diameter/stack/functional/s6t/S6tMonitoringType.java
@@ -0,0 +1,17 @@
+package org.mobicents.diameter.stack.functional.s6t;
+
+/**
+ * Created by Adi Enzel on 3/12/17.
+ *
+ * @author <a href="mailto:aa7133@att.com"> Adi Enzel </a>
+ */
+public class S6tMonitoringType {
+    public static final int LOSS_OF_CONNECTIVITY = 0;
+    public static final int UE_REACHABILITY = 1;
+    public static final int LOCATION_REPORTING = 2;
+    public static final int CHANGE_OF_IMSI_IMEI_ASSOCIATION = 3;
+    public static final int ROAMING_STATUS = 4;
+    public static final int COMMUNICATION_FAILURE = 5;
+    public static final int AVAILABILITY_AFTER_DDN_FAILURE = 6;
+    public static final int NUMBER_OF_UES_PRESENT_IN_A_GEOGRAPHICAL_AREA = 7;
+}
